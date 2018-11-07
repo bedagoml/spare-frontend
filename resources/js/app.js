80817988b6ece6414026ee51038ad75320853e8b
gibsonsilali@gmail.com <gibsonsilali@gmail.com>
2018-11-07 23:21:23 +0300

    that's how you get timbalan'd up

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,17 @@
+import Vue from "vue";
+import App from "./components/App";
+import router from "./router/index";
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import PaperDashboard from "./plugins/paperDashboard";
+import "vue-notifyjs/themes/default.css";
+
+Vue.use(PaperDashboard);
 
 require('./bootstrap');
 
-window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('app', App);
 
 const app = new Vue({
-    el: '#app'
-});
+    router,
+    render: h => h(App)
+}).$mount("#app");
---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Base CSS -->
+    <link rel="stylesheet" href="assets/css/basestyle/style.css">
+
+    <!-- Material Icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- Fontawesome Icons -->
+    <link href="{{ asset('public/assets/css/fontawesome/fontawesome-all.min.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('public/assets/css/pages/login.css') }}" rel="stylesheet">
+
+    <title>{{ config('app.name') }}</title>
+</head>
+<body>
+    <div id="app">
+        <app></app>
+    </div>
+    <script src="{{ asset('public/js/app.js') }}"></script>
+</body>
+</html>
